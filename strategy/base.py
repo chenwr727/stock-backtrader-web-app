cd7cd4fa9417d7d70767a9659c4aa350c26b8dbf
chenwr <worry369@163.com>
2022-02-18 11:49:53 +0800

    add base strategy

---
+++ strategy/base.py
@@ -0,0 +1,64 @@
+import backtrader as bt
+from utils.logs import LOGGER
+
+
+class BaseStrategy(bt.Strategy):
+    """base strategy"""
+
+    _name = "base"
+    params = (("printlog", False),)
+
+    def log(self, txt, dt=None, doprint=False):
+        """Logging function fot this strategy"""
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            LOGGER.info("%s, %s" % (dt.isoformat(), txt))
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    "BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f"
+                    % (order.executed.price, order.executed.value, order.executed.comm)
+                )
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log(
+                    "SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f"
+                    % (order.executed.price, order.executed.value, order.executed.comm)
+                )
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log("Order Canceled/Margin/Rejected")
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log("OPERATION PROFIT, GROSS %.2f, NET %.2f" % (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        pass
+
+    def stop(self):
+        params = [
+            f"{k}_{v}" for k, v in self.params._getkwargs().items() if k != "printlog"
+        ]
+        self.log(
+            "(%s %s) Ending Value %.2f"
+            % (self._name, " ".join(params), self.broker.getvalue()),
+            doprint=True,
+        )
--- strategy/macross.py
+++ strategy/macross.py
@@ -1,19 +1,59 @@
 import backtrader as bt
 
+from .base import BaseStrategy
 
-class MaCrossStrategy(bt.Strategy):
 
-    params = (("fast_length", 10), ("slow_length", 50))
+class MaCrossStrategy(BaseStrategy):
+
+    params = (("printlog", False), ("fast_length", 10), ("slow_length", 50))
 
     def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
         ma_fast = bt.ind.SMA(period=self.params.fast_length)
         ma_slow = bt.ind.SMA(period=self.params.slow_length)
 
         self.crossover = bt.ind.CrossOver(ma_fast, ma_slow)
 
     def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log("Close, %.2f" % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
         if not self.position:
             if self.crossover > 0:
                 self.buy()
         elif self.crossover < 0:
             self.close()
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.crossover > 0:
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log("BUY CREATE, %.2f" % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.crossover < 0:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log("SELL CREATE, %.2f" % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
